00390554797f0113a20e5ac9311f5262407c63e4
Andre Silva Bender <asbender4464@gmail.com>
2019-10-21 10:25:09 -0300

    Criando exceção personalizada e usando programação defensiva.

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -10,6 +10,10 @@ public class Tabuleiro {
 	
 	//Construtor padrão
 	public Tabuleiro(int linhas, int colunas) {
+		//O 'if' abaixo é característico de 'programação defensiva'.
+		if (linhas < 1 || colunas < 1) {
+			throw new TabuleiroExcecao("Erro na construção do tabuleiro: é preciso ter pelo menos 1 linha e 1 coluna.");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
@@ -20,33 +24,60 @@ public class Tabuleiro {
 		return linhas;
 	}
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
+	//Programação defensiva: retirando (comentando) o 'setLinhas', para não permitir mudar a quantidade de linhas.
+	//public void setLinhas(int linhas) {
+	//	this.linhas = linhas;
+	//}
 
 	public int getColunas() {
 		return colunas;
 	}
 
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
+	//Programação defensiva: retirando (comentando) o 'setColunas', para não permitir mudar a quantidade de colunas.
+	//public void setColunas(int colunas) {
+	//	this.colunas = colunas;
+	//}
 	
 	//Método para retornar a peça, dada uma linha e uma coluna
 	public Peca peca(int linha, int coluna) {
+		//Programação defensiva
+		if (!existePosicao(linha,coluna)) {
+			throw new TabuleiroExcecao("Posição inexistente no tabuleiro.");
+		}
 		return pecas[linha][coluna];
 	}
 	
 	//Sobrecarga do método, retornando a peça pela posição
 	public Peca peca (Posicao posicao) {
+		if (!existePosicao(posicao)) {
+			throw new TabuleiroExcecao("Posição inexistente no tabuleiro.");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 	
 	//Método 'Colocar Peca'.
 	public void colocarPeca (Peca peca, Posicao posicao) {
+		//Programação defensiva, para evitar sobreposição de peças.
+		if (haUmaPeca(posicao)) {
+			throw new TabuleiroExcecao("Já há uma peça nessa posição " + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		//É preciso informar que esta peça não está mais na posição 'nula'.
 		peca.posicao = posicao;
 	}
-	
+	//Método auxiliar ao 'Existe Posição'
+	public boolean existePosicao(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
+	//Método 'Existe Posição', valendo-se do método auxiliar definido acima.
+	public boolean existePosicao(Posicao posicao) {
+		return existePosicao(posicao.getLinha(),posicao.getColuna());
+	}
+	//Método 'Há uma peça'. Vale-se do método 'Peca', em sobrecarga, definido acima.
+	public boolean haUmaPeca(Posicao posicao) {
+		if (!existePosicao(posicao)) {
+			throw new TabuleiroExcecao("Posição inexistente no tabuleiro.");
+		}
+		return peca(posicao) != null;
+	}
 }
