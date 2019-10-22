25c65dff182ea4dda6c299e34ab52db59ee75348
Andre Silva Bender <asbender4464@gmail.com>
2019-10-22 14:32:34 -0300

    Tratando exceções e limpando a tela.

--- src/aplicacao/IU.java
+++ src/aplicacao/IU.java
@@ -32,6 +32,13 @@ public class IU {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	//Método para limpar a tela.
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+	
 	//Método para ler uma posição do usuário.
 	public static PosicaoXadrez lerPosicaoXadrez(Scanner sc) {
 		//Programação defensiva: bloco 'try-catch'
--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,12 @@
 package aplicacao;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import xadrez.PartidaDeXadrez;
 import xadrez.PecaDeXadrez;
 import xadrez.PosicaoXadrez;
+import xadrez.XadrezExcecao;
 
 public class Programa {
 
@@ -14,17 +16,30 @@ public class Programa {
 		PartidaDeXadrez partidaDeXadrez = new PartidaDeXadrez();
 		
 		while(true) { //Neste momento não há programação de cheque-mate e o programa ficará em 'loop', pelo parâmetro 'true'.
-		//IU = Interface do usuário, uma Classe.
-			IU.imprimaTabuleiro(partidaDeXadrez.getPecas());
-			System.out.println();
-			System.out.print("Origem: ");
-			PosicaoXadrez origem = IU.lerPosicaoXadrez(sc);
-			
-			System.out.println();
-			System.out.print("Destino: ");
-			PosicaoXadrez destino = IU.lerPosicaoXadrez(sc);
-			
-			PecaDeXadrez pecaCapturada = partidaDeXadrez.executarMovimentoXadrez(origem, destino);
+			try {
+				//Chamando o método de limpeza de tabuleiro antes de imprimi-lo.
+				IU.clearScreen();
+				
+				//IU = Interface do usuário, uma Classe.
+				IU.imprimaTabuleiro(partidaDeXadrez.getPecas());
+				System.out.println();
+				System.out.print("Origem: ");
+				PosicaoXadrez origem = IU.lerPosicaoXadrez(sc);
+				
+				System.out.println();
+				System.out.print("Destino: ");
+				PosicaoXadrez destino = IU.lerPosicaoXadrez(sc);
+				
+				PecaDeXadrez pecaCapturada = partidaDeXadrez.executarMovimentoXadrez(origem, destino);
+			}
+			catch(XadrezExcecao e) {
+				System.out.println(e.getMessage());
+				sc.nextLine(); //Esta linha faz o programa esperar até o usuário teclar ENTER.
+			}
+			catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine(); //Esta linha faz o programa esperar até o usuário teclar ENTER.
+			}
 		}
 	}
 }
